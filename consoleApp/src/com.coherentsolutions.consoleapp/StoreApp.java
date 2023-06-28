60c3a8af5cb9517acf554f3439c819b6515c4070
HalynaSereda <halynasereda@coherentsolutions.com>
2023-06-28 17:26:54 +0300

    Reflection Lib adding

--- consoleApp/src/com.coherentsolutions.consoleapp/StoreApp.java
+++ consoleApp/src/com.coherentsolutions.consoleapp/StoreApp.java
@@ -1,7 +1,20 @@
 package com.coherentsolutions.consoleapp;
 
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+import static org.reflections.scanners.Scanners.SubTypes;
+import static org.reflections.scanners.Scanners.TypesAnnotated;
+
 public class StoreApp {
     public static void main(String[] args){
+        Reflections reflections = new Reflections("onlinestore-HalynaSereda");
 
+        Set<Class<?>> subTypes =
+                reflections.get(SubTypes.of(SomeType.class).asClass());
+
+        Set<Class<?>> annotated =
+                reflections.get(SubTypes.of(TypesAnnotated.with(SomeAnnotation.class)).asClass());
     }
 }
