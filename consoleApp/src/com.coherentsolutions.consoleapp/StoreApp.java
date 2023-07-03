66ef7c42d7c7ce65c859bf95853553894afb7bbb
HalynaSereda <halynasereda@coherentsolutions.com>
2023-07-03 10:07:36 +0300

    RL error correction

--- consoleApp/src/com.coherentsolutions.consoleapp/StoreApp.java
+++ consoleApp/src/com.coherentsolutions.consoleapp/StoreApp.java
@@ -3,31 +3,33 @@ package com.coherentsolutions.consoleapp;
 import com.coherentsolutions.domain.Category;
 import org.reflections.Reflections;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
 
 public class StoreApp {
-    public static void main(String[] args) throws InstantiationException {
-
+    public static void main(String[] args) {
 
         Reflections reflections = new Reflections("com.coherentsolutions.domain");
         Set<Class<? extends Category>> subTypes = reflections.getSubTypesOf(Category.class);
 
         for (Class<? extends Category> subType : subTypes) {
             try {
-                Category instance = subType.getDeclaredConstructor().newInstance();
-               }
-            catch (InvocationTargetException e) {
-            e.printStackTrace();
-            } catch (NullPointerException  e) {
-            e.printStackTrace();
+                Constructor<? extends Category> constructor = subType.getDeclaredConstructor();
+                if (!constructor.isAccessible(null)) {
+                    System.out.printf("Cannot access constructor of %s, it might be private%n", subType.getSimpleName());
+                    continue;
+                }
+                Category instance = constructor.newInstance();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
             } catch (IllegalAccessException e) {
-            e.printStackTrace();
+                e.printStackTrace();
             } catch (NoSuchMethodException e) {
-            e.printStackTrace();}
-
+                e.printStackTrace();
             }
-               }
-
+        }
     }
-
+}
--- domain/src/com.coherentsolutions.domain/Category.java
+++ domain/src/com.coherentsolutions.domain/Category.java
@@ -11,7 +11,12 @@ public class Category {
         this.productList = new ArrayList<Product>();
     }
 
+
+
     public String getName() {
         return name;
+    }
+
+    public void setAccessible(boolean b) {
     }
 }
