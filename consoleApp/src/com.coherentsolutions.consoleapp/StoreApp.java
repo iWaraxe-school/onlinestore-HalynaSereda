160f87c8f9af6709994a196495405a738e9b8e25
HalynaSereda <halynasereda@coherentsolutions.com>
2023-08-02 19:37:12 +0300

    Store Comparator

--- consoleApp/src/com.coherentsolutions.consoleapp/StoreApp.java
+++ consoleApp/src/com.coherentsolutions.consoleapp/StoreApp.java
@@ -1,9 +1,14 @@
 package com.coherentsolutions.consoleapp;
 
+import com.coherentsolutions.domain.Category;
+import com.coherentsolutions.domain.Product;
+import com.coherentsolutions.sorting.StoreComparator;
 import com.coherentsolutions.store.Store;
 import com.coherentsolutions.store.StoreHelper;
 import com.coherentsolutions.xml.XMLParser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class StoreApp {
@@ -11,9 +16,28 @@ public class StoreApp {
         Store onlineStore = new Store();
         StoreHelper storeHelper = new StoreHelper(onlineStore);
         storeHelper.fillStore();
-        System.out.println(onlineStore);
-        Map<String, String > sortMap = XMLParser.getSortInOrder();
-        System.out.println(sortMap);
 
+        Map<String, String> sortingMap;
+        try {
+            sortingMap = XMLParser.getSortInOrder();
+        } finally {
+
+        }
+
+        StoreComparator storeComparator = new StoreComparator(sortingMap);
+
+        for (Category category : onlineStore.getCategoryList()) {
+            List<Product> products = new ArrayList<>(category.getProductList());
+            products.sort(storeComparator);
+
+            System.out.println("Category: " + category.getName());
+            for (Product product : products) {
+                System.out.println(product);
+            }
+            System.out.println();
+        }
     }
 }
+
+
+
--- store/src/main/java/com/coherentsolutions/sorting/StoreComparator.java
+++ store/src/main/java/com/coherentsolutions/sorting/StoreComparator.java
@@ -1,4 +1,47 @@
 package com.coherentsolutions.sorting;
 
-public class StoreComparator {
-}
+
+import com.coherentsolutions.domain.Product;
+
+import java.util.Comparator;
+import java.util.Map;
+
+
+
+public class StoreComparator implements Comparator<Product> {
+    Map<String, String> sortingMap;
+    public StoreComparator (Map<String, String> sortingMap)
+    {
+        this.sortingMap = sortingMap;
+    }
+    @Override
+    public int compare(Product o1, Product o2) {
+        for (Map.Entry<String, String> entry : sortingMap.entrySet()) {
+            String sortOrder = entry.getValue();
+            String sortField = entry.getKey();
+            int result = sortByField (sortField, o1, o2);
+            if (result != 0 ) {
+                if (sortOrder.equals("asc")) {
+                    return result;
+                } else if (sortOrder.equals("desc")) {
+                    return result * -1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private int sortByField (String sortField, Product o1, Product o2) {
+        switch (sortField) {
+            case "name":
+                return o2.getName().compareTo(o1.getName());
+            case "price":
+                return Double.compare(o1.getPrice(), o2.getPrice());
+            case "rate":
+                return Double.compare(o1.getRate(), o2.getRate());
+            default:  {
+                return 0;
+            }
+        }
+}}
