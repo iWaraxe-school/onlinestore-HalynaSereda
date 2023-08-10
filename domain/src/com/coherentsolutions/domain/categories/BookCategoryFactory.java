2cf2b8d9b602e8777d764af03632cdb6e4cfda2b
HalynaSereda <halynasereda@coherentsolutions.com>
2023-08-10 18:43:56 +0300

    added Singleton Pattern for Store, builder for Products and Factory for Categories

---
+++ domain/src/com/coherentsolutions/domain/categories/BookCategoryFactory.java
@@ -0,0 +1,11 @@
+package com.coherentsolutions.domain.categories;
+
+import com.coherentsolutions.domain.Category;
+import com.coherentsolutions.store.CategoryFactory;
+
+public class BookCategoryFactory implements CategoryFactory {
+    @Override
+    public Category createCategory() {
+        return new FoodCategory();
+    }
+}
