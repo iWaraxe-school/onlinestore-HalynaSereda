bc6aa3eec3c11f6eb542e8507f51a22f537fd129
HalynaSereda <halynasereda@coherentsolutions.com>
2023-08-28 13:22:37 +0300

    improvements for RandomStorePopulator.java and Store.java

--- store/src/com.coherentsolutions.store/RandomStorePopulator.java
+++ store/src/com.coherentsolutions.store/RandomStorePopulator.java
@@ -1,21 +1,21 @@
 package com.coherentsolutions.store;
 
-
 import com.coherentsolutions.domain.Product;
 import com.github.javafaker.Faker;
+
 /**
- * Generates random products for different categories using the Faker library.
+ * Class responsible for generating random products to populate the store.
  */
 public class RandomStorePopulator {
-    public static Faker faker = new Faker();
+    private static final Faker faker = new Faker();
+
     /**
-     * Generates a random product based on the specified category name.
+     * Generates a random product for the given category name.
      *
      * @param categoryName The name of the category for which to generate the product.
      * @return A randomly generated product.
-     * @throws IllegalArgumentException If the specified category name is not valid.
      */
-    public Product generateProduct(String categoryName) {
+    public synchronized Product generateProduct(String categoryName) {
         Product.ProductBuilder productBuilder = new Product.ProductBuilder();
 
         switch (categoryName) {
@@ -41,7 +41,7 @@ public class RandomStorePopulator {
                         .build();
 
             default:
-                throw new IllegalArgumentException("Unrecognized category: " + categoryName);
+                throw new IllegalArgumentException("No such category");
         }
     }
 }
