31bfb43dff046df1c4100ca687a3c8197a9039a7
HalynaSereda <halynasereda@coherentsolutions.com>
2023-09-04 08:25:24 +0300

    Http

--- store/src/com.coherentsolutions.store/HttpServer.java
+++ store/src/com.coherentsolutions.store/HttpServer.java
@@ -1,16 +1,11 @@
 package com.coherentsolutions.store;
 
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class HttpServer {
+    public void startServer(int port) throws IOException {
+    HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+    server.start();
+}
 }
